858f626a5b29124f5cda7f55d1f8ddcb68425f46
Marina Selivanova <mselivanova@ashion.com>
2025-05-05 10:40:30 +0200

    fix to string

--- src/main/java/com/quantori/chem_query_platform_demo/model/SearchResultResponse.java
+++ src/main/java/com/quantori/chem_query_platform_demo/model/SearchResultResponse.java
@@ -7,7 +7,6 @@ import lombok.ToString;
 
 import java.util.List;
 
-@ToString
 @Getter
 public record SearchResultResponse(String searchId, List<MoleculeDto> molecules, Long counter, Statistic statistic) {
     public SearchResultResponse(@JsonProperty("searchId") final String searchId,
